07c67a5f63d91fa686d2fb4cdf4f5c1bea90cf8b
Bashmakana <bashmakana@gmail.com>
2026-03-16 01:45:22 +0100

    Arduino

---
+++ CameraWebServer/CameraWebServer.ino
@@ -0,0 +1,97 @@
+#include "esp_camera.h"
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+#define CAMERA_MODEL_AI_THINKER
+#include "camera_pins.h"
+
+const char* ssid = "NESTO";
+const char* password = "pxqt9739";
+
+const char* serverURL = "http://192.168.13.231:5001/recognize"; 
+
+void initCamera() {
+  camera_config_t config;   
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer   = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sccb_sda = SIOD_GPIO_NUM;
+  config.pin_sccb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  config.frame_size = FRAMESIZE_QVGA;
+  config.jpeg_quality = 12;          
+  config.fb_count = 2;
+
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    while(1) delay(1000);
+  }
+  Serial.println("Camera OK");
+}
+
+void sendFrame() {
+  camera_fb_t * fb = esp_camera_fb_get();
+  if (!fb) {
+    Serial.println("Camera capture failed");
+    return;
+  }
+
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    http.begin(serverURL);
+    http.addHeader("Content-Type", "image/jpeg");
+
+    int httpResponseCode = http.POST(fb->buf, fb->len);
+
+    if (httpResponseCode > 0) {
+      String response = http.getString();
+      Serial.printf("HTTP %d | Faces: %s\n", httpResponseCode, response.c_str());
+    } else {
+      Serial.printf("HTTP Error: %s\n", http.errorToString(httpResponseCode).c_str());
+    }
+    http.end();
+  } else {
+    Serial.println("WiFi disconnected");
+  }
+
+  esp_camera_fb_return(fb);
+}
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi connected!");
+  Serial.print("ESP32 IP: ");
+  Serial.println(WiFi.localIP());  
+  Serial.print("Server URL: ");
+  Serial.println(serverURL);
+
+  initCamera();
+  delay(1000);
+}
+
+void loop() {
+  sendFrame();            
+  delay(50);            
+}
